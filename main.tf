2252714cb72ef78b9fe10f0791379462143321e2
Rajesh Radhakrishnan <>
2024-09-15 01:25:11 -0700

    updated

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 
 module "ssp_provision_bastion_host" {
-  source = "github.com/rajeshr264/ssp_provision_bastion_host"
- 
+  #source = "github.com/rajeshr264/ssp_provision_bastion_host"
+  source = "./work_modules/ssp_provision_bastion_host"
+
   bastion_host_password = var.bastion_host_password
   bastion_host_username = var.bastion_host_username
   bastion_host_public_key_filename = var.bastion_host_public_key_filename
